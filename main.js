852ec0a403d071fcca662eeae9d65c7f92b5c1d1
mentorvikash <mentorvikash@gmail.com>
2026-06-11 23:34:59 +0530

    Enhance worker thread implementation and update package.json to specify module type

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
+import { error } from 'console';
 import { Worker } from 'worker_threads'
 
-// create new worker
+// create new worker thread
 const worker = new Worker('./worker.js', {
-    workerData: { number: 10000000, id: 1 }
+    workerData: { number: 1000000000, id: 1 }
 });
+
+// Lister for the message from workers
+worker.on("message", (msg) => {
+    console.log(`result from worker ${msg.workerId}, message: ${msg.result}`)
+})
+
+// Handle error
+worker.on('error', () => {
+    console.log('worker error', error.message)
+})
+
+// Handle thread exit
+worker.on('exit', (code) => {
+    if (code !== 0) console.log(`worker stopped with exit code ${code}`);
+    console.log('worker finished successfully')
+})
+
+
+// Now will create multiple worker thread instead of one, and distribute the heavy work among them
+
+// import { Worker } from 'worker_threads'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
