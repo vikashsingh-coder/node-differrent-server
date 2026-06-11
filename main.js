98e94a90da22bb98f31a0efce7fc914c3be6f6f6
mentorvikash <mentorvikash@gmail.com>
2026-06-12 00:08:40 +0530

    Implement multi-threading for heavy calculations using worker threads

--- main.js
+++ main.js
@@ -1,39 +1,80 @@
-import { error } from 'console';
-import { Worker } from 'worker_threads'
-
-// create new worker thread
-const worker = new Worker('./worker.js', {
-    workerData: { number: 1000000000, id: 1 }
-});
-
-// Lister for the message from workers
-worker.on("message", (msg) => {
-    console.log(`result from worker ${msg.workerId}, message: ${msg.result}`)
-})
-
-// Handle error
-worker.on('error', () => {
-    console.log('worker error', error.message)
-})
-
-// Handle thread exit
-worker.on('exit', (code) => {
-    if (code !== 0) console.log(`worker stopped with exit code ${code}`);
-    console.log('worker finished successfully')
-})
-
-
-// Now will create multiple worker thread instead of one, and distribute the heavy work among them
-
+// import { error } from 'console';
 // import { Worker } from 'worker_threads'
 
+// // create new worker thread
+// const worker = new Worker('./worker.js', {
+//     workerData: { number: 1000000000, id: 1 }
+// });
+
+// // Lister for the message from workers
+// worker.on("message", (msg) => {
+//     console.log(`result from worker ${msg.workerId}, message: ${msg.result}`)
+// })
+
+// // Handle error
+// worker.on('error', () => {
+//     console.log('worker error', error.message)
+// })
+
+// // Handle thread exit
+// worker.on('exit', (code) => {
+//     if (code !== 0) console.log(`worker stopped with exit code ${code}`);
+//     console.log('worker finished successfully')
+// })
 
 
+// Now will create multiple worker thread instead of one, and distribute the heavy work among 3 worker equally
+
+import { Worker, threadId } from 'worker_threads'
+
+const TOTAL_NUM = 300000000;
+const NUM_WORKERS = 3;
+const CHUNK_SIZE = Math.ceil(TOTAL_NUM / NUM_WORKERS) // How many data handle by each worker
+
+console.log('Main Thread ID:', threadId)
+console.log('Main Process PID', process.pid)
+console.log('No of workers', NUM_WORKERS)
+
+let completed = 0;
+let totalsum = 0;
+
+// create 3 worker in our case
+for (let i = 0; i < NUM_WORKERS; i++) {
+
+    // creating state and end ranage
+    // first: 0 * 100000000 = 0
+    // second: 1 * 100000000 = 100000000
+    // third: 2 * 100000000 = 200000000
+    const start = i * CHUNK_SIZE;
+
+    // first: min of 100000000, 300000000    
+    // second: min of 100000000 + 100000000 = (20000000, 300000000)
+    // third: min of 200000000 + 100000000 = (300000000, 300000000)
+    const end = Math.min(start + CHUNK_SIZE, TOTAL_NUM)
+
+    const worker = new Worker('./worker.js', {
+        workerData: { start, end, taskId: i + 1 }  // 0, 100000000
+    })
+
+    // Capture result from worker
+    worker.on('message', (msg) => {
+        totalsum += msg.sum
+        completed++  // increase every time a worker give result
+
+        if (completed == NUM_WORKERS) { // all workers give result
+            console.log(`all workers done. final sum ${totalsum}`)
+            console.log(`Note: all thread share same process id ${process.pid}`)
+        }
+    })
+
+    // capture error
+    worker.on('error', (error) => {
+        console.log('error: ', error.message)
+    })
+}
 
 
-
-
-
+// let's run our main.js file
 
 
 
--- worker.js
+++ worker.js
@@ -1,15 +1,35 @@
-import { isMainThread, parentPort, workerData } from "worker_threads";
+// import { isMainThread, parentPort, workerData } from "worker_threads";
+
+// if (!isMainThread) {
+//     // Receive data from main thread
+//     const { number } = workerData
+
+//     // Perform heavy calculation
+//     let result = 0;
+//     for (let i = 0; i < number; i++) {
+//         result += i * 2
+//     }
+
+//     // Send the result to he main thread
+//     parentPort.postMessage({ result, workerId: workerData.id })
+// }
+
+// How to configure new workers
+import { isMainThread, parentPort, workerData, threadId } from 'worker_threads'
 
 if (!isMainThread) {
-    // Receive data from main thread
-    const { number } = workerData
+    const { start, end, taskId } = workerData;
+    console.log(`Thread ${threadId}, PID ${process.pid}, starting task ${taskId}`)
 
-    // Perform heavy calculation
-    let result = 0;
-    for (let i = 0; i < number; i++) {
-        result += i * 2
+    let sum = 0;
+
+    // sum all numbers in start and end range
+    for (let i = start; i < end; i++) {
+        sum += i;
     }
 
-    // Send the result to he main thread
-    parentPort.postMessage({ result, workerId: workerData.id })
+    console.log(`Thread ${threadId}, PID ${process.pid}, ending task ${taskId}`)
+
+    parentPort.postMessage({ taskId, sum, threadId })
+
 }
